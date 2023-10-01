6a60b40c933ea8d460da1429c6ca4b8a4463da09
Eddy Lujan <eddylujann@gmail.com>
2023-10-01 19:17:54 +0200

    Fix home

--- resources/views/web/stories/stories.blade.php
+++ resources/views/web/stories/stories.blade.php
@@ -3,56 +3,30 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col">
-                <div class="navbar-collapse">
-                    <div class="navbar-nav">
-                        <button class="border-0">
-                            <div>
-                                <a class="nav-link active" aria-current="page" href="#">
-                                    <img src="https://images.unsplash.com/photo-1695504236952-37306fc71896?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80"
-                                        class="img-fluid" alt="..."
-                                        style="border-radius: 50%; height: 77px; width: 77px;">
-                                </a>
-                            </div>
-                            <div>
-                                <span>username</span>
-                            </div>
-                        </button>
-                        <button class="border-0">
-                            <div>
-                                <a class="nav-link active" aria-current="page" href="#">
-                                    <img src="https://images.unsplash.com/photo-1695504236952-37306fc71896?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80"
-                                        class="img-fluid" alt="..."
-                                        style="border-radius: 50%; height: 77px; width: 77px;">
-                                </a>
-                            </div>
-                            <div>
-                                <span>username</span>
-                            </div>
-                        </button>
-                        <button class="border-0">
-                            <div>
-                                <a class="nav-link active" aria-current="page" href="#">
-                                    <img src="https://images.unsplash.com/photo-1695504236952-37306fc71896?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80"
-                                        class="img-fluid" alt="..."
-                                        style="border-radius: 50%; height: 77px; width: 77px;">
-                                </a>
-                            </div>
-                            <div>
-                                <span>username</span>
-                            </div>
-                        </button>
-                        <button class="border-0">
-                            <div>
-                                <a class="nav-link active" aria-current="page" href="#">
-                                    <img src="https://images.unsplash.com/photo-1695504236952-37306fc71896?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80"
-                                        class="img-fluid" alt="..."
-                                        style="border-radius: 50%; height: 77px; width: 77px;">
-                                </a>
-                            </div>
-                            <div>
-                                <span>username</span>
-                            </div>
-                        </button>
+                <div class="d-flex flex-row">
+                    <div class="p-2 text-center">
+                        <a class="nav-link active" aria-current="page" href="#">
+                            <img src="https://images.unsplash.com/photo-1695504236952-37306fc71896?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80"
+                                class="img-fluid" alt="..."
+                                style="border-radius: 50%; height: 77px; width: 77px;">
+                        </a>
+                        <a href="" class="text-black text-decoration-none">username</a>
+                    </div>
+                    <div class="p-2 text-center">
+                        <a class="nav-link active" aria-current="page" href="#">
+                            <img src="https://images.unsplash.com/photo-1695504236952-37306fc71896?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80"
+                                class="img-fluid" alt="..."
+                                style="border-radius: 50%; height: 77px; width: 77px;">
+                        </a>
+                        <a href="" class="text-black text-decoration-none">username</a>
+                    </div>
+                    <div class="p-2 text-center">
+                        <a class="nav-link active" aria-current="page" href="#">
+                            <img src="https://images.unsplash.com/photo-1695504236952-37306fc71896?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80"
+                                class="img-fluid" alt="..."
+                                style="border-radius: 50%; height: 77px; width: 77px;">
+                        </a>
+                        <a href="" class="text-black text-decoration-none">username</a>
                     </div>
                 </div>
             </div>
