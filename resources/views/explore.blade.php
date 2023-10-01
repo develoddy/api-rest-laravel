1d398551cc4ca99423658beb30827a685b680b48
Eddy Lujan <eddylujann@gmail.com>
2023-10-02 00:35:28 +0200

    fix explorer

--- resources/views/explore.blade.php
+++ resources/views/explore.blade.php
@@ -3,12 +3,142 @@
 
 @section('content')
 <div class="container">
-    <div class="row mt-2 mb-0 justify-content-center">
-        <div class="col col-md-10 col-lg-8 col-sm-12 col-xl-6 p-0">
-            <div class="d-flex flex-column">
-                <div class="col bg-success">item</div>
-                <div class="col bg-primary">item</div>
+    <div class="row mt-2 mb-0 justify-content-center border border-primary">
+        <div class="col col-md-10 col-lg-8 col-sm-12 col-xl-10 p-2">
+            <div class="row">
+                {{-- Primera columna --}}
+                <div class="col-4">
+                    <div class="element d-flex h-100 flex-column justify-content-between">
+                        <div class="inner d-flex h-100 justify-content-center">
+                            <img class="img-fluid img-thumbnail border-0 rounded-0" src="https://source.unsplash.com/sesveuG_rNo/400x300" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-8">
+                    <div class="row">
+                        <div class="col">
+                            <div class="element d-flex h-100 flex-column justify-content-between">
+                                <div class="inner d-flex h-100 justify-content-center">
+                                    <img class="img-fluid img-thumbnail border-0 rounded-0" src="https://source.unsplash.com/sesveuG_rNo/400x300" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="element d-flex h-100 flex-column justify-content-between">
+                                <div class="inner d-flex h-100 justify-content-center">
+                                    <img class="img-fluid img-thumbnail border-0 rounded-0" src="https://source.unsplash.com/sesveuG_rNo/400x300" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                
+                    <div class="row">
+                        <div class="col">
+                            <div class="element d-flex h-100 flex-column justify-content-between">
+                                <div class="inner d-flex h-100 justify-content-center">
+                                    <img class="img-fluid img-thumbnail border-0 rounded-0" src="https://source.unsplash.com/sesveuG_rNo/400x300" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="element d-flex h-100 flex-column justify-content-between">
+                                <div class="inner d-flex h-100 justify-content-center">
+                                    <img class="img-fluid img-thumbnail border-0 rounded-0" src="https://source.unsplash.com/sesveuG_rNo/400x300" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {{-- Segunda columna --}}
+                <div class="col-8">
+                    <div class="row">
+                        <div class="col">
+                            <div class="element d-flex h-100 flex-column justify-content-between">
+                                <div class="inner d-flex h-100 justify-content-center">
+                                    <img class="img-fluid img-thumbnail border-0 rounded-0" src="https://source.unsplash.com/sesveuG_rNo/400x300" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="element d-flex h-100 flex-column justify-content-between">
+                                <div class="inner d-flex h-100 justify-content-center">
+                                    <img class="img-fluid img-thumbnail border-0 rounded-0" src="https://source.unsplash.com/sesveuG_rNo/400x300" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                
+                    <div class="row">
+                        <div class="col"><div class="element d-flex h-100 flex-column justify-content-between">
+                            <div class="inner d-flex h-100 justify-content-center">
+                                <img class="img-fluid img-thumbnail border-0 rounded-0" src="https://source.unsplash.com/sesveuG_rNo/400x300" alt="">
+                            </div>
+                        </div></div>
+                        <div class="col"><div class="element d-flex h-100 flex-column justify-content-between">
+                            <div class="inner d-flex h-100 justify-content-center">
+                                <img class="img-fluid img-thumbnail border-0 rounded-0" src="https://source.unsplash.com/sesveuG_rNo/400x300" alt="">
+                            </div>
+                        </div></div>
+                    </div>
+                </div>
+
+                <div class="col-4">
+                    <div class="element d-flex h-100 flex-column justify-content-between">
+                        <div class="inner d-flex h-100 justify-content-center">
+                            <img class="img-fluid img-thumbnail border-0 rounded-0" src="https://source.unsplash.com/sesveuG_rNo/400x300" alt="">
+                        </div>
+                    </div>
+                </div>
+
+                
+                
+                
+
+
+                {{-- <div class="col-md-8 bg-primary">
+                    columm1
+                    <div class="row">
+                      <div class="col-md-6 bg-danger">.col-md-6</div>
+                      <div class="col-md-6 bg-success">.col-md-6</div>
+                    </div>
+                  </div>
+                  <div class="col-md-4 bg-warning">.col-md-4</div>
+                </div> --}}
+
+                
+
+                {{-- <div class="col-4 p-2">
+                    <div class="element d-flex h-100 flex-column justify-content-between">
+                        <div class="inner d-flex h-100 flex-column align-items-center justify-content-center">
+                            <img class="img-fluid img-thumbnail border-0 rounded-0" src="https://source.unsplash.com/sesveuG_rNo/400x300" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4 p-2">
+                    <div class="element d-flex h-100 flex-column justify-content-between">
+                        <div class="inner d-flex h-100 flex-column align-items-center justify-content-center">
+                            <img class="img-fluid img-thumbnail border-0 rounded-0" src="https://source.unsplash.com/sesveuG_rNo/400x300" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4 p-2">
+                    <div class="element d-flex h-100 flex-column justify-content-between">
+                        <div class="inner d-flex h-100 flex-column align-items-center justify-content-center">
+                            <img class="img-fluid img-thumbnail border-0 rounded-0" src="https://source.unsplash.com/sesveuG_rNo/400x300" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4 p-2">
+                    <div class="element d-flex h-100 flex-column justify-content-between">
+                        <div class="inner d-flex h-100 flex-column align-items-center justify-content-center">
+                            <img class="img-fluid img-thumbnail border-0 rounded-0" src="https://source.unsplash.com/sesveuG_rNo/400x300" alt="">
+                        </div>
+                    </div>
+                </div> --}}
             </div>
+
+            
         </div>
     </div>
 </div>
