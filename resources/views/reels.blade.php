40c609dc1b73aa8f3e0228ac2d3f1a8963948398
Eddy Lujan <eddylujann@gmail.com>
2023-10-02 09:57:57 +0200

    fix template reels

---
+++ resources/views/reels.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+@section('title', 'Profile')
+
+@section('content')
+<div class="container">
+    <div class="row mt-2 mb-0 justify-content-center">
+        <div class="col col-md-10 col-lg-8 col-sm-12 col-xl-10 p-2">
+            <div class="row">
+
+            </div>
+        </div>
+    </div>  
+</div>
